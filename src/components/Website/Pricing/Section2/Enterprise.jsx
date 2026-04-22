6cf7a2c27446e3e2cb95667f057632eaf0123fcb
Your Name <your.email@example.com>
2026-04-22 13:28:28 +0100

    minor edit on solutions page

--- src/components/Website/Pricing/Section2/Enterprise.jsx
+++ src/components/Website/Pricing/Section2/Enterprise.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import styles from "./styles.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Enterprise() {
   return (
@@ -9,24 +10,26 @@ export default function Enterprise() {
       <h2>Every enterprise is unique.</h2>
       <p>We offer tailored pricing based on your operational requirements. </p>
       <div>
-        <button className={styles.demo}>
-          Request Custom Pricing{" "}
-          <svg
-            width='18'
-            height='16'
-            viewBox='0 0 18 16'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M1 8H17M10 1L17 8L10 15'
-              stroke='#FFFFFF'
-              stroke-width='2'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            />
-          </svg>
-        </button>
+        <NavLink to='/contact'>
+          <button className={styles.demo}>
+            Request Custom Pricing{" "}
+            <svg
+              width='18'
+              height='16'
+              viewBox='0 0 18 16'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M1 8H17M10 1L17 8L10 15'
+                stroke='#FFFFFF'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+              />
+            </svg>
+          </button>
+        </NavLink>
       </div>
     </div>
   );
--- src/components/Website/Solutions/Section2/Expert.jsx
+++ src/components/Website/Solutions/Section2/Expert.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import styles from "./styles.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Expert() {
   return (
@@ -9,24 +10,26 @@ export default function Expert() {
       <h2>Talk to an Expert</h2>
       <p>Learn how Uppist can transform your customer operations. </p>
       <div>
-        <button className={styles.demo}>
-          Contact Sales{" "}
-          <svg
-            width='18'
-            height='16'
-            viewBox='0 0 18 16'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M1 8H17M10 1L17 8L10 15'
-              stroke='#FFFFFF'
-              stroke-width='2'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            />
-          </svg>
-        </button>
+        <NavLink to='/contact'>
+          <button className={styles.demo}>
+            Contact Sales{" "}
+            <svg
+              width='18'
+              height='16'
+              viewBox='0 0 18 16'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='M1 8H17M10 1L17 8L10 15'
+                stroke='#FFFFFF'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+              />
+            </svg>
+          </button>
+        </NavLink>
       </div>
     </div>
   );
